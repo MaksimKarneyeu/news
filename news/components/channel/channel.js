379558b1cf670d431488f36d200b8881110d894a
Maksim Karneyeu <maksim_karneyeu@epam.com>
2018-11-14 17:30:42 +0300

    fix fetch

--- news/components/channel/channel.js
+++ news/components/channel/channel.js
@@ -5,11 +5,10 @@ const channelsUrl = `${Config.channelsEndpoint}?apiKey=${Config.apiKey}`;
 const newsChannelsId = "news-channels";
 const positionToPasteChannel = "afterbegin";
 
-fetch(channelsUrl)
-    .then(data => {
-        let newsChannel = document.getElementById(newsChannelsId);
+fetch(channelsUrl).then(response => response).then(data => {
+    let newsChannel = document.getElementById(newsChannelsId);
 
-        data.json().sources.sort((first, second) => second.name.localeCompare(first.name)).map(channel =>
-            newsChannel.insertAdjacentHTML(positionToPasteChannel, render(channel)))
-    })
+    data.json().sources.sort((first, second) => second.name.localeCompare(first.name)).map(channel =>
+        newsChannel.insertAdjacentHTML(positionToPasteChannel, render(channel)))
+})
     .catch(error => { alert(error) });
