a8cb284879a8411622880ac54f27cd184cfc2152
Maksim Karneyeu <maksim_karneyeu@epam.com>
2018-11-14 17:01:45 +0300

    fix paths

--- news/components/news/channel/channel.template.js
+++ news/components/news/channel/channel.template.js
@@ -1,5 +1,5 @@
 export function render(channel) {
-  return `<a href=\"../news/components/news/news/news.html?name=${channel.name}&id=${channel.id}\" class=\"list-group-item\">
+  return `<a href=\"/news/news/components/news/news/news.html?name=${channel.name}&id=${channel.id}\" class=\"list-group-item\">
           <b>${channel.name}<b>
           <br>
           <small>${channel.description}</small></a`
