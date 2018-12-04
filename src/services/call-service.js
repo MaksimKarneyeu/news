7e889cb5ad278d061fa478637f725dcb64053f62
Maksim Karneyeu <maksim_karneyeu@epam.com>
2018-12-04 16:46:39 +0300

    update for 4

--- src/services/call-service.js
+++ src/services/call-service.js
@@ -1,6 +1,11 @@
-export default class CallManager {
-    static doGet(url) {
+import BaseCallService from "./base-call-service.js";
+
+export default class CallService extends BaseCallService {
+    constructor(){
+        super();
+    }
+    doGet(url) {
         return fetch(url).then(async (response) => !response.ok ?
-         await Promise.reject(response.statusText) : response.json());
+            await Promise.reject(response.statusText) : response.json());
     }
 }
--- src/widgets/channel-widget/channel-widget.js
+++ src/widgets/channel-widget/channel-widget.js
@@ -1,40 +1,36 @@
 import RenderItem from "./channel-item-widget_template.js";
-import CallService from "../../services/call-service.js";
 import Config from "../../config.json";
-import Init from "../../widget-initializer.js";
 
 export default class ChannelWidget {
-    constructor(container) {
-        this.container = container;
-        this.channelsUrl = `${Config.channelsEndpoint}?apiKey=${Config.apiKey}`;   
-    }  
+    constructor(data, controller) {     
+        this.data = data;      
+        this.controller = controller;
+    }
 
     setRenderElements(items) {
         this.items = items;
     }
 
-    async bootstrap() {
-        const data = await this.loadData();
-        this.render(data);
+    async bootstrap() {      
+        this.render(this.data);
         this.initEvents();
-    }
-
-    async loadData() {
-        const response = await CallService.doGet(this.channelsUrl);
-        const data = await response;
-        return !response.ok ? data.sources : [];
-    }
+    }    
 
     render(data) {
         data.map(element => this.items.innerHTML += RenderItem(element));
     }
 
     initEvents() {
-        let elements = this.items.childNodes;
+        const elements = this.items.childNodes;       
         for (let index = 0; index < elements.length; index++) {
-            elements[index].onclick = async () => {
-                const newsWidget = Init['news-widget'](elements[index]);
-                await newsWidget.bootstrap();
+            elements[index].onclick = async () => {  
+                const element = elements[index];         
+                const channel = {
+                    channelId: element.getAttribute(Config.channel.channelId),
+                    channelName: element.getAttribute(Config.channel.channelName)
+                  }     
+                const channelWidget = await this.controller.getNews(channel);
+                await channelWidget.bootstrap();
             }
         }
     }
